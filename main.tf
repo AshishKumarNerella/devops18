1f168b63fee96fd1a84fdd6bf9d628c558c9c696
AshishKumarNerella <nerellaashish.js@gmail.com>
2025-02-05 19:41:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_launch_template" "web_server_as" {
     name = "myproject"
-    image_id           = "ami-0454e52560c7f5c55"
+    image_id           = "ami-04681163a08179f28"
     vpc_security_group_ids = [aws_security_group.web_server.id]
     instance_type = "t2.micro"
-    key_name = "lastone"
+    key_name = "MyLinuxPractice-KeyPair"
     tags = {
         Name = "DevOps"
     }
@@ -15,7 +15,7 @@ resource "aws_launch_template" "web_server_as" {
   resource "aws_elb" "web_server_lb"{
      name = "web-server-lb"
      security_groups = [aws_security_group.web_server.id]
-     subnets = ["subnet-024286466e258f339", "subnet-09558309acf4661a8"]
+     subnets = ["subnet-0153896f7a6b4c46a", "subnet-086838fc4eb7a46ea"]
      listener {
       instance_port     = 8000
       instance_protocol = "http"
