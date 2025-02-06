4cafcbadc8db86061656515c1cbb78a3f3cb6911
AshishKumarNerella <nerellaashish.js@gmail.com>
2025-02-06 15:30:36 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -22,3 +22,10 @@ versioning_configuration {
 status = "Enabled"
 }
 }
+terraform {
+backend "s3" {
+region = "us-east-1"
+bucket = "ashish.devops.monolithicproject.bucket"
+key = "prod/terraform.tfstate"
+}
+}
